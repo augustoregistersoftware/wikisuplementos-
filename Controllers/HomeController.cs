17af3e9469e73467c3937a1681dd1ac5e2721e30
augustoregistersoftware <augusto.valenciano@fabricadecodigos.com.br>
2024-06-11 13:43:38 -0300

    SPRINT adicionado insert no site

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using wikisuplementos.Models;
+using wikisuplementos.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace wikisuplementos.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, AppDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -18,22 +24,23 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Suplementos()
+    public async Task<IActionResult> Suplementos()
+    {
+        var suplementos = await _context.Suplementos.ToListAsync();
+        return View(suplementos);
+    }
+
+    public IActionResult AtletasTreinadores()
     {
         return View();
     }
 
-      public IActionResult AtletasTreinadores()
+    public IActionResult CadastroSuplmentos()
     {
         return View();
     }
 
-        public IActionResult CadastroSuplmentos()
-    {
-        return View();
-    }
-
-        public IActionResult CadastroAtletasTreinadores()
+    public IActionResult CadastroAtletasTreinadores()
     {
         return View();
     }
